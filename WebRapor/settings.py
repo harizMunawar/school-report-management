49cfd0a8c742a2e5a30df8eb91b654c1273d6dee
harizMunawar <munawarhariz@gmail.com>
2020-09-02 20:02:12 +0700

    Heroku Configuration

--- WebRapor/settings.py
+++ WebRapor/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = '09(7c9yo7(ui323%-)#eyd6asgmiho&_eh=-#ypi=vrp6w8uig'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://django-covid.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'https://manage-report.herokuapp.com/']
 
 
 # Application definition
--- WebRapor/urls.py
+++ WebRapor/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
 from REST.routers import router
+from django.conf import settings
+from django.conf.urls.static import static
+from django.views.static import serve 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('', include('django.contrib.auth.urls')),
     path('', include('User.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
