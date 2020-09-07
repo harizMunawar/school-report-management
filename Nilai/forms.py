ff36aadb6e60022eb52515f9dae55f71f4e0714e
harizMunawar <munawarhariz@gmail.com>
2020-09-07 12:15:09 +0700

    Add insert and update Nilai in Guru dashboard

--- Nilai/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from Nilai.models import NilaiMataPelajaran
-
-class NilaiForm(forms.ModelForm):
-
-    class Meta:
-        model = NilaiMataPelajaran
-        exclude = ['siswa', 'pelajaran']
