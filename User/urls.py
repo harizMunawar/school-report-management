3355034008ef40d4a6faab69aa23dcec811c9412
harizMunawar <munawarhariz@gmail.com>
2020-09-20 16:47:31 +0700

    Changing all regex-path with path

--- User/urls.py
+++ User/urls.py
@@ -1,17 +1,17 @@
-from django.urls import path, re_path
+from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.dashboard, name="dashboard"),
-    re_path(r'^register(?:/(?P<level>[\w-]+))*/$', views.Registration.as_view(), name='registration'),        
+    path('register/<level>/', views.Registration.as_view(), name='registration'),
     path('create/<level>/', views.CreateUser.as_view(), name='create-user'),
-    re_path(r'^delete/user/(?P<nomor_induk>[\w-]+)/$', views.DeleteUser.as_view(), name='delete-user'),
+    path('delete/user/<nomor_induk>/', views.DeleteUser.as_view(), name='delete-user'),
 
     path('siswa/', views.ListSiswa.as_view(), name='list-siswa'),
-    re_path(r'^siswa/(?P<nomor_induk>[\w-]+)/$', views.EditSiswa.as_view(), name='siswa'),
+    path('siswa/<nomor_induk>/', views.EditSiswa.as_view(), name='siswa'),
 
     path('guru/', views.ListGuru.as_view(), name='list-guru'),
-    re_path(r'^guru/(?P<nomor_induk>[\w-]+)/$', views.EditGuru.as_view(), name='guru'),
+    path('guru/<nomor_induk>/', views.EditGuru.as_view(), name='guru'),
 
     path('bulk-insert/', views.bulk_insert, name='bulk-insert'),
 ]
