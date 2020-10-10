1f848ec282b1383ebdbcfa494c1e0d66f819e554
unknown <munawarhariz@gmail.com>
2020-10-10 20:38:18 +0700

    Refactor Helpers function code

--- Helpers/__init__.py
+++ Helpers/__init__.py
@@ -1,33 +1,25 @@
 from Nilai.models import MataPelajaran, NilaiMataPelajaran
 
+def nilai(id, siswa):
+    nil = NilaiMataPelajaran.objects.values('nilai').filter(pelajaran=id, siswa=siswa)
+    if nil:
+        for nil in nil:
+            return nil['nilai']
+    else:
+        return 0    
+
 def zip_pelnilai(siswa):        
     matapelajaran = MataPelajaran.objects.values('id', 'nama').filter(kelas=siswa.kelas)[::1]
     list_id = [pelajaran['id'] for pelajaran in matapelajaran]
     list_pelajaran = [pelajaran['nama'] for pelajaran in matapelajaran]            
-    list_nilai = []
-
-    for pelajaran in matapelajaran:
-        nil = NilaiMataPelajaran.objects.values('nilai').filter(pelajaran=pelajaran['id'], siswa=siswa)
-        if not nil:
-            list_nilai.append(0)
-        else:
-            for nil in nil:
-                list_nilai.append(nil['nilai'])
+    list_nilai = [nilai(pelajaran['id'], siswa) for pelajaran in matapelajaran]
 
     return zip(list_id, list_pelajaran, list_nilai)
 
 def get_nilai(siswa):
     matapelajaran = MataPelajaran.objects.values('id').filter(kelas=siswa.kelas)[::1]
-    list_nilai = []
-    
-    for pelajaran in matapelajaran:
-        nil = NilaiMataPelajaran.objects.values('nilai').filter(pelajaran=pelajaran['id'], siswa=siswa)
-        if not nil:
-            list_nilai.append(0)
-        else:
-            for nil in nil:
-                list_nilai.append(nil['nilai'])
-    return list_nilai    
+   
+    return [nilai(pelajaran['id'], siswa) for pelajaran in matapelajaran]   
 
 def zip_siswa_status(list_siswa):    
     return [(siswa, False) if 0 in get_nilai(siswa) else (siswa, True) for siswa in list_siswa]    
